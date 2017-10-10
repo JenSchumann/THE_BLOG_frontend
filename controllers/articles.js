50038e4255cd46bc7e406bece82478dd81d389ef
Jen Schumann <jenniferschumann1@gmail.com>
2017-10-10 10:04:19 -0600

    added controller and schema

---
+++ controllers/articles.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const Article = require('../models/articles.js');
+
+//index
+router.get('/', (req, res)=>{
+  Article.find({}, (err, foundArticles)=>{
+    res.json(foundArticles);
+  });
+});
+
+//create
+router.post('/', (req, res)=>{
+  console.log(req.body)
+  Article.create(req.body, (err, createdArticle)=>{
+    res.json(createdArticle);
+  });
+});
+
+//update
+router.put('/:id', (req, res)=>{
+  Article.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedArticle)=>{
+    res.json(updatedArticle);
+  });
+});
+
+//delete
+router.delete('/:id', (req, res)=>{
+  Article.findByIdAndRemove(req.params.id, (err, deletedArticle)=>{
+    res.json(deletedArticle);
+  });
+});
+
+module.exports = router;
