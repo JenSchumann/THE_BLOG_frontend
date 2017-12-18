b244d09db163f051ef12148cc8628ec63d1d5dc5
Jen Schumann <jenniferschumann1@gmail.com>
2017-12-17 18:51:08 -0700

    added register link on login page

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const Author = require('./authors.js');
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
